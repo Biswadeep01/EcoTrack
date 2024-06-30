d32119e15f23c19213f0b67522f9d22231d51ac6
Biswadeep Mukhopadhyay <97559458+Biswadeep01@users.noreply.github.com>
2024-06-30 07:44:45 +0530

    Final Commit

---
+++ frontendStreamLit/DataAnalysis/csvtographfinal.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_data():
+    # Step 1: Read the CSV file into a pandas DataFrame
+    df = pd.read_csv('DataAnalysis/predicted_emissions.csv')
+
+    # Step 2: Extract x and y values from the DataFrame
+    days = df['Date']
+    values = df['Predicted Carbon Emission']
+
+    # Step 3: Create the plot
+    fig, ax = plt.subplots(figsize=(7, 4))
+
+    ax.plot(days, values, marker='o', linestyle='-')
+
+    # Step 4: Customize the plot
+    ax.set_title('Predicted Carbon Emission according to date')
+    ax.set_xlabel('Days')
+    ax.set_ylabel('Value')
+    ax.grid(True)
+
+    # Step 5: Display the plot in Streamlit
+    st.pyplot(fig)
+    return True
+
+
